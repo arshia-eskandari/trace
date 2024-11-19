c456548461abf69e08b34a5ef3b500fdad48703f
arshia-eskandari <arshia.eskandari.3000@gmail.com>
2024-11-19 13:31:54 -0500

    added tests

---
+++ tests/cli.rs
@@ -0,0 +1,28 @@
+use assert_cmd::Command;
+use testresult::TestResult;
+
+#[test]
+fn status_code_is_error_if_no_command_is_specified() -> TestResult {
+    Command::cargo_bin("track")?.assert().failure();
+    Ok(())
+}
+
+#[test]
+fn start_command_starts_tracking_time() -> TestResult {
+    Command::cargo_bin("track")?.arg("start").assert().success();
+    Ok(())
+}
+
+#[test]
+fn stop_command_stops_tracking_time() -> TestResult {
+    Command::cargo_bin("track")?.arg("stop").assert().success();
+    todo!();
+}
+
+#[test]
+fn report_command_generates_report() -> TestResult {
+    Command::cargo_bin("track")?.arg("start").assert().success();
+    Command::cargo_bin("track")?.arg("stop").assert().success();
+    Command::cargo_bin("track")?.arg("report").assert().stdout("00:00:00\n").success();
+    Ok(())
+}
