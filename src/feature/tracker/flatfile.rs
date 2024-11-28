f8b73a8854fc5948f76de6fc3d2858804db9024d
arshia-eskandari <arshia.eskandari.3000@gmail.com>
2024-11-28 11:09:48 -0500

    removed comments

--- src/feature/tracker/flatfile.rs
+++ src/feature/tracker/flatfile.rs
@@ -17,12 +17,10 @@ impl FlatFileTracker {
     }
 
     pub fn start(&self) -> Result<(), String> {
-        // Check if the lockfile exists
         if Path::new(&self.lockfile).exists() {
             return Err("The timer must be stopped before initiation".to_owned());
         }
 
-        // Create or open the lockfile
         let lockfile = OpenOptions::new()
             .write(true)
             .create(true)
@@ -30,11 +28,9 @@ impl FlatFileTracker {
             .expect("Failed to create or open the lock file");
         lockfile.lock_exclusive().expect("Failed to acquire lockfile");
 
-        // Get the current timestamp
         let now = Local::now();
         let timestamp = now.timestamp();
 
-        // Create or open the JSON file (db_dir)
         let mut db_file = OpenOptions::new()
             .write(true)
             .create(true)
@@ -42,22 +38,17 @@ impl FlatFileTracker {
             .open(&self.db_dir)
             .expect("Failed to create or open the JSON file");
 
-        // Read existing JSON data, if any
-        let mut db_content = String::new();
-        db_file.read_to_string(&mut db_content).expect("Failed to read the JSON file");
+        let mut buffer= String::new();
+        db_file.read_to_string(&mut buffer).expect("Failed to read the JSON file");
 
-        let mut data = if db_content.trim().is_empty() {
-            // If file is empty, start with an empty JSON object
+        let mut data = if buffer.trim().is_empty() {
             json!({})
         } else {
-            // Parse existing JSON data
-            serde_json::from_str(&db_content).expect("Failed to parse the JSON file")
+            serde_json::from_str(&buffer).expect("Failed to parse the JSON file")
         };
 
-        // Add or update the `timestamp` field
         data["timestamp"] = json!(timestamp);
 
-        // Write the updated JSON data back to the file
         db_file.set_len(0).expect("Failed to truncate the JSON file"); // Clear file contents
         db_file.write_all(data.to_string().as_bytes()).expect("Failed to write to the JSON file");
 
